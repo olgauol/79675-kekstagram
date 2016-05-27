d00320d13a5667dfd2085295a8987740d124942e
Olga <olga.uol@gmail.com>
2016-05-27 22:09:59 +0300

    Переделанный скрипт по 2 заданию.

--- build/js/check.js
+++ build/js/check.js
@@ -1,46 +1,31 @@
-function getMessage(a, b)
-{
-    var typeOfA = typeof a;
-    var typeOfB = typeof b;
-
-    if (typeOfA == "boolean")
-    {
-        return ("Переданное GIF-изображение анимировано и содержит " + b + " кадров");
-    }
-     else
-    {
-        return ("Переданное GIF-изображение не анимировано");
-    }
-
-    if (typeOfA == "number") 
-    {
-        return ("Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов");
-    }
-
-    if (Array.isArray(a))
-    { 
-        return ("Количество красных точек во всех строчках изображения: " + 
-            function sumA(a)
-            {
-                for (var i = 0; i < a.value; i++)
-                {
-                    a += a;
-                }
-            }
-            )
-    }
-    if (Array.isArray(a) && Array.isArray(b))
-    {
-        return ("Общая площадь артефактов сжатия: " + 
-            function squareAB(a, b)
-            {
-                for (var i = 0; i < (a * b); (a * b)++)
-                {
-                    a * b;
-                }
-            }
-
-         + " пикселей")
-    }
-
+function getMessage(a, b){
+  var typeOfA = typeof a;
+  var sumA = 0;
+  var squareAB = 0;
+  if (typeOfA == 'boolean') {
+    if (a == true) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    } 
+  } 
+  else if (typeOfA == 'number') {
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+  } 
+  else if (Array.isArray(a)) {
+    if (Array.isArray(b)) {
+      for (var i = 0; i < b.length; i++) {
+        squareAB += a[i] * b[i];
+      }
+    return 'Общая площадь артефактов сжатия: ' + squareAB;
+    } 
+    else {
+      for (var i = 0; i < b.length; i++) {
+        sumA += a[i];
+      }
+      return 'Количество красных точек во всех строчках изображения: ' + sumA;
+    } 
+  }
+  else
+  {
+    return 'Переданное GIF-изображение не анимировано';
+  }
 }
